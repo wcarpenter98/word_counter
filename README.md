041b5c549e7574b162bf52136444556d00156e5d
wcarpenter98 <50806560+wcarpenter98@users.noreply.github.com>
2023-03-13 01:04:08 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# word_counter
+# simple_word_counter
+I used a website similar to this throughout college to analyze my word choice count. I threw this together on a Sunday. Perhaps I will revisit it one day and clean it up. 
+
+https://wcarpenter98.github.io/word_counter/
---
+++ app.js
@@ -0,0 +1,139 @@
+function myFunction() {
+  var textAreaText = document.getElementById('textArea').value;
+  var arrayOfWords = textAreaText.split(" ");
+  var wordSet = new Set(arrayOfWords);
+  var arrayOfWordsNoRepeats = Array.from(wordSet);
+  getWordCount(arrayOfWords);
+  getCharacterCount(arrayOfWords);
+  getStats(arrayOfWords, arrayOfWordsNoRepeats);
+  return arrayOfWords;
+}
+
+function getWordCount(wordArray){
+  //count all of the words except spaces
+  var totalWords = 0;
+  for(var i = 0; i < wordArray.length; i++){
+    if(wordArray[i] != " " && wordArray[i] != ''){
+      totalWords += 1;
+    }
+  }
+  document.getElementById("wordCount").innerHTML = String(totalWords);
+}
+
+function getCharacterCount(wordArray){
+  //count all of the characters that arent empty stings or spaces or periods.
+  var totalCharacters = 0;
+  for(var i = 0; i < wordArray.length; i++){
+    if(wordArray[i] != " " && wordArray[i] != ''){
+      //now count all of the characters of the current word
+      totalCharacters += wordArray[i].length;
+    }
+  }
+  //TODO: ABOVE COULD BE USED FOR SIGNIFICANT CHARACTERS AKA CHARACTERS WITHOUT SPACES.
+  document.getElementById("sigCharacterCount").innerHTML = totalCharacters;
+  document.getElementById("characterCount").innerHTML = String(document.getElementById('textArea').value.length);
+}
+
+function getStats(totalWordArray, totalWordArrayNoRepeats){
+  //create a hashmap counting all of the words occurrences
+  const hashMap = new Map();
+  //now go through the list of totalWords with repeats and add to the hashMap totals
+  for(var i = 0; i < totalWordArray.length; i++){
+    var currentWord = totalWordArray[i];
+    //does word end in special character
+    if(doesWordEndInSpecialCharacter(currentWord)){
+      currentWord = currentWord.substring(0, currentWord.length -1);
+    }
+    //check if that hashMap has a value
+    if(hashMap.get(currentWord)){
+      var currentTotal = hashMap.get(currentWord);
+      hashMap.set(currentWord, currentTotal+1)
+    }
+    else{
+      hashMap.set(currentWord, 1);
+    }
+  }
+  //now all words are mapped to their frequencies
+  // very -> 4 
+  // a -> 1
+  //now take all of the words and order them from greatest to least based on their frequencies
+
+
+  //map the frequency -> listOfWords
+  var frequencyMap = new Map();
+
+  for (var [key, value] of hashMap.entries()) {
+    if(frequencyMap.get(value) && frequencyMap.get(value) != ''){
+      var currentWordList = frequencyMap.get(value);
+      currentWordList.push(key)
+      frequencyMap.set(value, currentWordList);
+    }
+    else{
+      var wordList = [];
+      //TODO: CHECK WHERE THESE EMPTY KEYS ARE COMING FROM, THIS IS A PATCHY FIX BELOW WITH THE IF STATEMENT
+      if(key != ''){
+        wordList.push(key);
+        frequencyMap.set(value, wordList);
+      }
+    }
+  }
+
+  //find the highest frequency
+  var highestFrequency = -1;
+  for (var [key, value] of hashMap.entries()) {
+    if(value > highestFrequency){
+      highestFrequency = value
+    }
+  }
+
+
+  //now start from the highest frequency and increment down each time in a loop
+  //put the highest frequency words in a list like this 
+  //[word] occurence# (#percentage)
+  //sky 7 indivdual occurences (10% of total words)
+  
+  var detailedStatsList = []
+  for(var i = highestFrequency; i > 0; i--){
+    //if a wordlist exist with that frequency match
+    if(frequencyMap.get(i)){
+      //loop thru the word list and add the modifed string to the detailedStatsList
+      var currentWordList = frequencyMap.get(i);
+      for(var j = 0; j < currentWordList.length; j++){
+        var currentWord = currentWordList[j];
+        //derive stats for the word
+        //currentWord (7 occurences) (10 percent of total words)
+        var percentage = i/totalWordArray.length * 100
+        currentWord = currentWord + " (" + i + " occurences) (" + percentage.toFixed(3) + "% of total words)";
+        detailedStatsList.push(currentWord)
+      }
+    }
+  }
+
+
+
+
+ // var statsTextBox = document.getElementById("statsText");
+
+  //create a string to put inside of the stats text box
+  var statsTextString = "";
+  for(var i = 0; i < detailedStatsList.length; i++){
+    statsTextString += detailedStatsList[i] + "\n" + "\n"
+  }
+
+
+  //TODO: do this with a new element below. 
+  document.getElementById("theStats").innerText = statsTextString;
+  console.log(frequencyMap)
+
+}
+
+function doesWordEndInSpecialCharacter(word){
+  //! . , ? ; : ' " 
+  if(word[word.length-1] == '!' || word[word.length-1] == '.' || word[word.length-1] == ',' || word[word.length-1] == '?' || word[word.length-1] == ';' || word[word.length-1] == ':' || word[word.length-1] == '\'' || word[word.length-1] == '\"'){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
